8798da6fde4ce5b4d31c47724e6c2d66aa381012
juanjaviermino <juan.mino.arboleda@udla.edu.ec>
2024-11-08 00:28:19 -0500

    Add generate static params to arcandina page

--- app/[lang]/arcandina/page.js
+++ app/[lang]/arcandina/page.js
@@ -1,6 +1,15 @@
 import WorkingScreen from '@/arcandina/WorkingScreen';
 import { getDictionary } from '@/dictionaries/dictionaries.js';
 
+export const dynamicParams = false;
+
+export async function generateStaticParams() {
+  return [
+    { lang: 'en' },
+    { lang: 'es' },
+  ];
+}
+
 export default async function Home({ params }) {
 
   const dict = await getDictionary(params.lang);
