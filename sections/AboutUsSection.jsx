63cb881aeb627425d3534660909bd3abf4432b89
juanjaviermino <juan.mino.arboleda@udla.edu.ec>
2024-11-27 00:20:44 -0500

    Finish new section in about us

--- sections/AboutUsSection.jsx
+++ sections/AboutUsSection.jsx
@@ -1,5 +1,4 @@
 import LinkText from '@/components/LinkText';
-import TeamCard from '@/components/TeamCard';
 import WindowCarousel from '@/components/WindowCarousel';
 import React from 'react';
 
@@ -21,13 +20,26 @@ const AboutUsSection = ({dict}) => {
             <img className='aboutus-section__banderines' src='/banderines-image.webp' alt='An image of some flags on a rope'></img>
             <img className='aboutus-section__banderines--small' src='/banderines-image-small.webp' alt='An image of some flags on a rope'></img>
         </div>
-        
-        <span className='aboutus-section__content-title'>
-            {dict.aboutUsText6}
-        </span>
-        <div className='aboutus-section__creators'>
-            <TeamCard imageSrc={'/maria-elena-ordonez-img.jpg'} title={'María Elena Ordoñez'} description={dict.mariaElenaDescription}/>
-            <TeamCard imageSrc={'/pablo-palacios-img.jpg'} title={'Pablo Palacios Naranjo'} description={dict.pabloDescription}/>
+        <div className='aboutus-section__logos'>
+            <img className='aboutus-section__logos--arcandina' src='/arcandina-logo-image.webp' alt='Arcandina logo'></img>
+            <img className='aboutus-section__logos--rotary' src='/rotary-logo-image.webp' alt='Rotary logo'></img>
+        </div>
+        <h3 className='aboutus-section__title--second'>
+            <span>{dict.aboutUsText6}</span>
+            <span>{dict.aboutUsText7}</span>
+        </h3>
+        <div className='aboutus-section__text' style={{paddingBottom: '40px'}}>
+            <div className='aboutus-section__shelf'>
+                <div className='aboutus-section__description aboutus-section__description--reversed'>
+                    <div className='aboutus-section__description__overlay'></div>
+                    <img className='aboutus-section__description__image' src='/characters/cori.webp' alt='An image of a travelling suitcase'></img>
+                    <p className='aboutus-section__description__text'>
+                        {dict.aboutUsText8}
+                        <br></br>  <br></br>
+                        {dict.aboutUsText9}
+                    </p>
+                </div>
+            </div>
         </div>
         <span className='aboutus-section__content-title'>
             {dict.aboutUsText4}
