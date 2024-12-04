fc9467dc42c566b4e97c398a77adee78f0360c4f
juanjaviermino <juan.mino.arboleda@udla.edu.ec>
2024-12-03 21:21:02 -0500

    Add flipbook component for magazine reading

---
+++ components/Flipbook.jsx
@@ -0,0 +1,99 @@
+'use client';
+
+import React, { useImperativeHandle, useState, useEffect, forwardRef } from 'react';
+import HTMLFlipBook from "react-pageflip";
+import { Dialog } from 'primereact/dialog';
+
+const Flipbook = forwardRef(({ pages }, ref) => {
+    const aspectRatio = 1.4179; // Define the aspect ratio (landscape default)
+    const [visible, setVisible] = useState(false);
+    const [flipbookHeight, setFlipbookHeight] = useState(0);
+    const [flipbookWidth, setFlipbookWidth] = useState(flipbookHeight / aspectRatio);
+    const [isPortrait, setIsPortrait] = useState(false);
+
+    // Expose the setVisible method to the parent using useImperativeHandle
+    useImperativeHandle(ref, () => ({
+        open: () => setVisible(true),
+        close: () => setVisible(false),
+    }));
+
+    const updateFlipbookDimensions = () => {
+        const screenHeight = window.innerHeight;
+        const screenWidth = window.innerWidth;
+
+        // Set height to 80% of the viewport height
+        let height = Math.round(screenHeight * 0.9);
+        let width = Math.round(height / aspectRatio);
+
+        // If width exceeds the screen width, adjust dimensions
+        if(screenWidth < 768){
+            if (width > screenWidth * 0.9) { // Allow a maximum width of 90% of the screen width
+                width = Math.round(screenWidth * 0.9);
+                height = Math.round(width * aspectRatio); // Recalculate height based on aspect ratio
+            }
+        } else{
+            if ((width * 2)  > screenWidth * 0.9) { // Allow a maximum width of 90% of the screen width
+                width = Math.round(screenWidth * 0.9 / 2);
+                height = Math.round(width * aspectRatio); // Recalculate height based on aspect ratio
+            }
+        }
+        
+
+        setFlipbookHeight(height);
+        setFlipbookWidth(width);
+
+        // Switch to portrait mode if screen width is below 768px
+        setIsPortrait(screenWidth < 768);
+    };
+
+    useEffect(() => {
+        // Initial dimensions calculation
+        updateFlipbookDimensions();
+
+        // Listen for resize events
+        window.addEventListener('resize', updateFlipbookDimensions);
+
+        return () => {
+            window.removeEventListener('resize', updateFlipbookDimensions);
+        };
+    }, []);
+
+    return (
+        <Dialog
+            className='flipbook__dialog'
+            header=""
+            visible={visible}
+            draggable={false}
+            onHide={() => setVisible(false)}
+            resizable={false}
+        >
+            <div className='flipbook__container'>
+                <HTMLFlipBook
+                    width={flipbookWidth}
+                    height={flipbookHeight}
+                    showCover={false}
+                    drawShadow={true}
+                    flippingTime={1000}
+                    className='flipbook__book'
+                    usePortrait={isPortrait}
+                >
+                    {pages.map((src, index) => (
+                        <div key={index} className="page">
+                            <img
+                                src={src}
+                                alt={`Page ${index + 1}`}
+                                style={{
+                                    width: '100%',
+                                    height: '100%',
+                                    objectFit: 'cover', // Ensure images fit nicely
+                                }}
+                            />
+                        </div>
+                    ))}
+                </HTMLFlipBook>
+            </div>
+        </Dialog>
+    );
+});
+
+export default Flipbook;
--- components/content-displayers/LandingPageRevista.jsx
+++ components/content-displayers/LandingPageRevista.jsx
@@ -1,48 +1,60 @@
-import React from 'react'
+'use client'
+
+import React, {useRef} from 'react'
 import Button from '../Button'
 import Image from 'next/image'
+import Flipbook from '@/components/Flipbook'
+import revistaPages from '@/data/revista/rotary-revista.json'
 
 const LandingPageRevista = ({dict}) => {
-  
 
-  return (
-    <section className="content-displayer__revista">
-        <div className='content-displayer__revista__container'>
-            <div className='content-displayer__revista__info'>
-                <div className='content-displayer__revista__cover'>
+    const flipbookRef = useRef(null);
+
+    const openFlipbook = () => {
+        if (flipbookRef.current) {
+            flipbookRef.current?.open();
+        }
+    };
+
+    return (
+        <section className="content-displayer__revista">
+            <div className='content-displayer__revista__container'>
+                <div className='content-displayer__revista__info'>
+                    <div className='content-displayer__revista__cover'>
+                        <Image
+                            className='content-displayer__revista__cover__image'
+                            src="/revista-cover.webp" 
+                            alt="Portada de la revista A bordo del Arcandina"
+                            width={800} 
+                            height={800} 
+                            priority 
+                        />
+                    </div>
+                    <div className='content-displayer__revista__text'>
+                        <h6>{dict.revistaTitle}</h6>
+                        <p>{dict.revistaDescription}</p>
+                        <Button 
+                            actionType='function'
+                            onClick={openFlipbook}
+                            label={dict.revistaButton}
+                            icon='BookOpenText'
+                        />
+                    </div>
+                </div>
+                <div className='content-displayer__revista__image'>
                     <Image
-                        className='content-displayer__revista__cover__image'
-                        src="/revista-cover.webp" 
-                        alt="Portada de la revista A bordo del Arcandina"
+                        className='content-displayer__revista__image__image'
+                        src="/revista-image.webp" 
+                        alt="Imagen de una parte de la revista A bordo del Arcandina"
                         width={800} 
                         height={800} 
                         priority 
                     />
                 </div>
-                <div className='content-displayer__revista__text'>
-                    <h6>{dict.revistaTitle}</h6>
-                    <p>{dict.revistaDescription}</p>
-                    <Button 
-                        actionType='redirect'
-                        onClick={'https://drive.google.com/file/d/11M8kjjn3Ml8pGCDavMC9dfCrfn84447j/view'}
-                        label={dict.revistaButton}
-                        icon='BookOpenText'
-                    />
-                </div>
             </div>
-            <div className='content-displayer__revista__image'>
-                <Image
-                    className='content-displayer__revista__image__image'
-                    src="/revista-image.webp" 
-                    alt="Imagen de una parte de la revista A bordo del Arcandina"
-                    width={800} 
-                    height={800} 
-                    priority 
-                />
-            </div>
-        </div>
-    </section>
-  )
+            <Flipbook ref={flipbookRef} pages={revistaPages.pages || []}/>
+        </section>
+    )
 }
 
 export default LandingPageRevista
