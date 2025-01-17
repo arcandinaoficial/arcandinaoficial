b5e1aaf5b99d2228dc39de14a1f106bdca46148c
juanjaviermino <juan.mino.arboleda@udla.edu.ec>
2025-01-17 12:04:37 -0500

    Improve main page SEO

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@
 import LoadingScreen from '@/screens/LoadingScreen';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import Head from 'next/head';
 
 export default function Home() {
   const router = useRouter();
@@ -21,6 +22,17 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <title>Arcandina Oficial | Educación ambiental para niños</title>
+        <meta name="description" content="Arcandina es el portal educativo líder en educación ambiental para niños, ofreciendo material audiovisual interactivo sobre la conservación del Río San Pedro, y la importancia de las buenas prácticas ambientales. Descubre a personajes como Jagui, Cori, Ratasura, Tucán y Guardián Verde, quienes acompañan a los niños en su aprendizaje. Ideal para cualquier búsqueda relacionada con conservación ambiental." />
+        <meta name="keywords" content=" Ratasura, Arcandina, Oficial, Cori, Jagui, Jagüi, Medio ambiente, Rio, Agua, Educación ambiental, Fundación Arcandina" />
+        <meta name="author" content="Arcandina" />
+        <meta property="og:title" content="Arcandina Oficial" />
+        <meta property="og:description" content="Aprende con en el Arcandina cómo cuidar los recursos de nuestro planeta" />
+        <meta property="og:url" content="https://arcandinaoficial.org/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://arcandinaoficial.org/apple-touch-icon.png" />
+      </Head>
       <LoadingScreen />
     </>
   );
